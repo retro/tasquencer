9acff7ca5bb1f9887fedb47e1e477a5954ad223c
Mihael Konjevic <konjevic@gmail.com>
2024-09-08 12:43:02 +0200

    fix: type-fest must be included in the dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   test: {
     include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    coverage: {
+      provider: 'istanbul',
+      include: ['src/**/*.{ts,tsx}'],
+    },
   },
 });
